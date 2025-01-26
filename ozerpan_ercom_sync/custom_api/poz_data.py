60a284275c8e61165ec7ec722df050cb1f48cfb5
hsrvms <59848940+hsrvms@users.noreply.github.com>
2025-01-26 10:33:38 +0300

    regroup get_poz_data response

--- ozerpan_ercom_sync/custom_api/poz_data.py
+++ ozerpan_ercom_sync/custom_api/poz_data.py
@@ -1,34 +1,79 @@
 import frappe
 
+# ITEM_GROUPS = {
+#     "destek_saci": [
+#         "Pvc Destek Sacları",
+#     ],
+#     "satis": [
+#         "Pvc Satış",
+#         "Satış Stoğu",
+#     ],
+#     "yardimci_profil": [
+#         "Pvc Hat1 Yardımcı Profiller",
+#         "Pvc Hat2 Yardımcı Profiller",
+#     ],
+#     "panel": [
+#         "Pvc Hat1 Paneller",
+#     ],
+#     "ana_profil": [
+#         "Pvc Hat1 Ana Profiller",
+#         "Pvc Hat2 Ana Profiller",
+#     ],
+#     "cita": [
+#         "Pvc Hat1 Çıtalar",
+#     ],
+#     "aksesuar": [
+#         "Pvc Hat1 Aksesuarlar",
+#         "Pvc Hat2 Aksesuarlar",
+#     ],
+#     "orta_kayit": [
+#         "Pvc Hat1 Ortakayıt Bağlantı",
+#         "Pvc Hat2 Ortakayıt Bağlantı",
+#     ],
+#     "fitil_ve_esik": [
+#         "Pvc Hat1 Fitil ve Eşikler",
+#     ],
+#     "cam_unite": [
+#         "Cam Üniteler",
+#         "Cam Ünite Üretim",
+#         "Cam Ünite Malzemeler",
+#         "Cam Ünite Kimyasallar",
+#         "Cam Ünite Karolaj Çıtaları",
+#         "Cam Ünite Ara Çıtalar",
+#         "Tek Camlar",
+#     ],
+#     "ortak_fitil": [
+#         "Ortak Fitiller",
+#     ],
+#     "ortak_vida": [
+#         "Ortak Vidalar",
+#     ],
+#     "ortak_izolasyon": [
+#         "Ortak İzolasyon",
+#     ],
+#     "pvc_kollari": [
+#         "Pvc Kolları",
+#     ],
+#     "aluminyum_dograma_sistemi": [
+#         "Alüminyum Doğrama Sistem Aks",
+#     ],
+# }
+
 
 @frappe.whitelist()
 def get_poz_data(barcode: str):
     print("\n\n\n-- Get Poz Data API --")
 
-    print("Barcode:", barcode)
     tesdetay = frappe.get_doc("TesDetay", {"barkod": barcode})
 
     bom_name = f"BOM-{tesdetay.siparis_no}-{tesdetay.poz_no}"
     bom_doc = get_latest_default_bom(bom_name)
     bom_item_doc = frappe.get_doc("Item", f"{tesdetay.siparis_no}-{tesdetay.poz_no}")
 
-    print(f"{bom_item_doc}")
-
-    print("BOM fields:")
-    for field in bom_doc.items[0].__dict__:
-        print(f"{field}: {bom_doc.items[0].get(field)}")
-
-    # print("BOM Item fields:")
-    # for field in bom_item_doc.__dict__:
-    #     print(f"{field}: {bom_item_doc.get(field)}")
-    # print(f"\n{bom_item_doc.image}\n")
-
     grouped_items = group_bom_items_by_category(bom_doc)
 
     if bom_doc.custom_accessory_kit:
         kit = frappe.get_doc("Item", bom_doc.custom_accessory_kit)
-        print("\n\nKit:", kit)
-        print("bom_doc_qty:", bom_doc.custom_accessory_kit_qty)
         grouped_items["accessory_kit"] = [
             {
                 "item_code": kit.item_code,
@@ -37,11 +82,6 @@ def get_poz_data(barcode: str):
                 "image": kit.image,
             }
         ]
-
-    for group, items in grouped_items.items():
-        print(f"\n{group}")
-        for item in items:
-            print(item)
 
     data = {
         "siparis_no": tesdetay.get("siparis_no"),
@@ -59,20 +99,55 @@ def get_poz_data(barcode: str):
 
 def group_bom_items_by_category(bom_doc):
     """Group BOM Items by their item_group."""
+    ITEM_GROUPS = {
+        "destek_saci": ["pvc destek sacları"],
+        "satis": ["pvc satış", "satış stoğu"],
+        "yardimci_profil": ["pvc hat1 yardımcı profiller", "pvc hat2 yardımcı profiller"],
+        "panel": ["pvc hat1 paneller"],
+        "ana_profil": ["pvc hat1 ana profiller", "pvc hat2 ana profiller"],
+        "cita": ["pvc hat1 çıtalar"],
+        "aksesuar": ["pvc hat1 aksesuarlar", "pvc hat2 aksesuarlar"],
+        "orta_kayit": ["pvc hat1 ortakayıt bağlantı", "pvc hat2 ortakayıt bağlantı"],
+        "fitil_ve_esik": ["pvc hat1 fitil ve eşikler"],
+        "cam_unite": [
+            "cam ünite",
+            "cam üniteler",
+            "cam ünite üretim",
+            "cam ünite malzemeler",
+            "cam ünite kimyasallar",
+            "cam ünite karolaj çıtaları",
+            "cam ünite ara çıtalar",
+            "tek camlar",
+        ],
+        "ortak_fitil": ["ortak fitiller"],
+        "ortak_vida": ["ortak vidalar"],
+        "ortak_izolasyon": ["ortak izolasyon"],
+        "pvc_kollari": ["pvc kolları"],
+        "aluminyum_dograma_sistemi": ["alüminyum doğrama sistem aks"],
+    }
     grouped_items = {}
 
     for item in bom_doc.items:
         item_group = frappe.get_value("Item", item.item_code, "item_group")
-        if item_group not in grouped_items:
-            grouped_items[item_group] = []
-        grouped_items[item_group].append(
-            {
-                "item_code": item.item_code,
-                "item_name": item.item_name,
-                "quantity": item.qty,
-                "image": item.image,
-            }
-        )
+        normalized_group = item_group.lower()
+
+        target_group = None
+        for group_key, group_values in ITEM_GROUPS.items():
+            if any(value in normalized_group for value in group_values):
+                target_group = group_key
+                break
+
+        if target_group:
+            if target_group not in grouped_items:
+                grouped_items[target_group] = []
+            grouped_items[target_group].append(
+                {
+                    "item_code": item.item_code,
+                    "item_name": item.item_name,
+                    "quantity": item.qty,
+                    "image": item.image,
+                }
+            )
 
     return grouped_items
 
