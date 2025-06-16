01cb08181430dc3e6f63e6ea119c80eb4a85529e
hsrvms <59848940+hsrvms@users.noreply.github.com>
2025-06-16 11:35:05 +0300

    Add process-file button into prodcuction plan, remove unnecessary codes from sales_order_list.js

--- ozerpan_ercom_sync/public/js/production_plan/production_plan_list.js
+++ ozerpan_ercom_sync/public/js/production_plan/production_plan_list.js
@@ -1,60 +1,22 @@
 frappe.listview_settings["Production Plan"] = {
   refresh(listview) {
-    console.log("-- ListView Refresh --");
+    console.log("-- ListView refresh --");
   },
   onload(listview) {
     console.log("-- ListView Onload --");
-    uploadExcel(listview);
+    fileProcessor(listview);
   },
 };
 
-function uploadExcel(listview) {
-  listview.page.add_inner_button(__("Upload OPT/DST"), () => {
-    let d = new frappe.ui.Dialog({
-      title: __("Select Excel File"),
-      fields: [
-        {
-          label: "File",
-          fieldname: "file",
-          fieldtype: "Attach",
-          reqd: 1,
-        },
-      ],
-      size: "small",
-      primary_action_label: __("Submit"),
-      primary_action(values) {
-        console.log(values);
-        callUploadFileApi(values);
-        d.hide();
+function fileProcessor(listview) {
+  listview.page.add_inner_button(__("Process File"), () => {
+    frappe.call({
+      method: "ozerpan_ercom_sync.custom_api.api.process_file",
+      callback: function (r) {
+        if (r.message) {
+          frappe.msgprint(r.message);
+        }
       },
     });
-    d.show();
-  });
-}
-
-function callUploadFileApi(values) {
-  frappe.call({
-    method: "ozerpan_ercom_sync.custom_api.file_upload.upload_file.upload_file",
-    args: {
-      file_url: values.file,
-    },
-    freeze: true,
-    callback: (r) => {
-      if (r.message) {
-        console.log(r.message);
-        frappe.msgprint({
-          title: __("Success"),
-          indicator: "green",
-          message: __("File processed successfully."),
-        });
-      }
-    },
-    error: (r) => {
-      frappe.msgprint({
-        title: __("Error"),
-        indicator: "red",
-        message: __("An error occurred while processing the file."),
-      });
-    },
   });
 }
