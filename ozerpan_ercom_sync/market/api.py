b47107a9ff88c7fc04d0f79d285caa72b768596f
hsrvms <59848940+hsrvms@users.noreply.github.com>
2025-10-25 15:40:01 +0300

    create market order using the data coming from market frontend

--- ozerpan_ercom_sync/market/api.py
+++ ozerpan_ercom_sync/market/api.py
@@ -1,11 +1,17 @@
 from typing import Any, Dict, List, Optional, TypedDict
 
 import frappe
+from frappe import _
+
+from ozerpan_ercom_sync.market.sales_order.service import create_market_sales_order
+from ozerpan_ercom_sync.market.utils import get_user_customer_details
 
 
 class SalesOrder(TypedDict):
     name: str
-    # Add other fields that might be returned by get_list
+
+
+# Add other fields that might be returned by get_list
 
 
 @frappe.whitelist()
@@ -33,3 +39,39 @@ def get_ercom_orders(search_filter: Optional[str] = None) -> Dict[str, List[Sale
 @frappe.whitelist()
 def sales_order(data):
     return data
+
+
+@frappe.whitelist()
+def create_sales_order(data):
+    print("\n\n-- Create Sales Order (Market) -- START\n")
+    response: dict[str, any] = {}
+    customer = get_user_customer_details(frappe.session.user)
+
+    if not customer:
+        frappe.throw(_("Customer not found."))
+
+    try:
+        ercom_sales_order = frappe.get_doc("Sales Order", data.get("order_no"))
+
+    except frappe.DoesNotExistError as e:
+        print(f"Ercom Sales Order not found. Error: {e}")
+        ercom_sales_order = None
+
+    response.update({"ercom_sales_order": ercom_sales_order})
+
+    new_sales_order_result = create_market_sales_order(
+        ercom_sales_order=ercom_sales_order,
+        customer=customer,
+        market_order_data=data,
+    )
+
+    if new_sales_order_result.get("status") == "error":
+        frappe.local.response["http_status_code"] = 400
+        frappe.local.response["message"] = new_sales_order_result
+        return
+
+    print("\n-- Create Sales Order (Market) -- END\n\n")
+    return {
+        "status": "success",
+        "data": data,
+    }
